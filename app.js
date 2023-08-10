d89e346a6d757a516c28cdb1d28b214868fccf79
tomstins1 <tomstins1@gmail.com>
2023-08-10 13:57:53 +0100

    pdf and ppt button

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 console.log("what a life");
+
+let viz;
+
 // 1. create a variable to store our vizcontainer
 
 const vizBox = document.getElementById("vizContainer");
@@ -19,9 +22,33 @@ const url =
 //   4. define our function to build dashboard
 
 function initViz() {
-  const viz = new tableau.Viz(vizBox, url, options);
+  viz = new tableau.Viz(vizBox, url, options);
 }
 
 // 5. controlling when fuction is run - only runs after everything else is loaded from HTML
 
 document.addEventListener("DOMContentLoaded", initViz);
+
+// 6. Adding PDF export functionality
+
+document.addEventListener("DOMContentLoaded", function () {
+  const exportPdfbutton = document.getElementById("exportPdf");
+
+  exportPdfbutton.addEventListener("click", exportPdfFunction);
+
+  function exportPdfFunction() {
+    viz.showExportPDFDialog();
+  }
+});
+
+// 7. adding Ppt export button
+
+document.addEventListener("DOMContentLoaded", function () {
+  const exportPptbutton = document.getElementById("exportPpt");
+
+  exportPptbutton.addEventListener("click", exportPptFunction);
+
+  function exportPptFunction() {
+    viz.showExportPowerPointDialog();
+  }
+});
